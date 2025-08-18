18b1acb409e615384366aedf0f04d70e2f482d85
Jose <henrique.cg@outlook.com>
2025-08-18 18:30:20 -0300

    Primeiro exercício aula 02

--- Exercícios_intermediário_imd/Programação_estruturada/Aula 02/Atividade_03_2/Atividade_03_2.js
+++ Exercícios_intermediário_imd/Programação_estruturada/Aula 02/Atividade_03_2/Atividade_03_2.js
@@ -1,29 +1,29 @@
-//var valorTotal     = 1;
+//var valorTotal     = 1; //válido
 //document.getElementById("saida").innerHTML = valorTotal;
 
-//var valortotal     = 2;
+//var valortotal     = 2; //válido
 //document.getElementById("saida").innerHTML = valortotal;
 
-//var valor_total    = 3;
+//var valor_total    = 3; //válido
 //document.getElementById("saida").innerHTML = valor_total;
 
-//var 1_valor_total  = 4;
+//var 1_valor_total  = 4; //inválido
 //document.getElementById("saida").innerHTML = 1_valor_total;
 
-//var valor_total_1  = 5;
+//var valor_total_1  = 5; //válido
 //document.getElementById("saida").innerHTML = valor_total_1;
 
-//var $valor_total   = 6;
+//var $valor_total   = 6; //válido
 //document.getElementById("saida").innerHTML = $valor_total;
 
-//var _valor_total   = 7;
+//var _valor_total   = 7; //válido 
 //document.getElementById("saida").innerHTML = _valor_total;
 
-//var valor-total    = 8;
+//var valor-total    = 8; //inválido
 //document.getElementById("saida").innerHTML = valor-total;
 
-//var function       = 9;
+//var function       = 9; //inválido
 //document.getElementById("saida").innerHTML = function;
 
-//var object       = 9;
-//document.getElementById("saida").innerHTML = object;
+var Object       = 9;
+document.getElementById("saida").innerHTML = Object;

d6d5a6b149197c5ddbe4a8e6c5d0de71c9e1401e
Jose <henrique.cg@outlook.com>
2025-08-18 18:45:05 -0300

    exercício 03

---
+++ Exercícios_intermediário_imd/Programação_estruturada/Aula 02/Atividade_03_4/Atividade_03_4.js
@@ -0,0 +1,4 @@
+        function soma_e_escreve() {
+            var x = Number(document.getElementById("entrada1").value), y = Number(document.getElementById("entrada2").value), z = x + y;    
+            document.getElementById("saida").innerHTML = z;
+        }

e72293276549b0044c00f5f1dd498efd8ddcf9aa
Jose <henrique.cg@outlook.com>
2025-09-22 19:02:34 -0300

    Questão 07 terminada Aula 11

---
+++ Exercícios_intermediário_imd/Programação_estruturada/Aula 11/Atividade_11_7/Atividade_11_7.js
@@ -0,0 +1,45 @@
+var array1 = [];
+var array2 = [];
+var soma = [];
+exibir();
+
+function inserir(a) {
+    if(a=== 1){
+        var num1 = Number(document.getElementById("entrada1").value);
+        array1.push(num1);
+    } else if (a===2){
+        var num2 = Number(document.getElementById("entrada2").value);
+        array2.push(num2);
+    }
+    somar();
+    exibir();
+}
+
+function somar() {
+    soma = [];  
+    if(array1.length>array2.length){
+        for(i=0; i< array2.length; i++){
+            soma.push(array1[i]+array2[i]);
+        }
+
+        for(i=array2.length; i<array1.length; i++){
+            soma.push(array1[i]);
+        }
+    } else {
+        for(i=0; i<array1.length ;i++){
+            soma.push(array1[i]+array2[i]);
+        }
+
+        for(i= array1.length; i<array2.length; i++){
+            soma.push(array2[i]);
+        }
+    }
+
+}
+
+function exibir() {
+    var texto = "Array 1 = [" + array1 + "] <br>";
+    texto = texto + "Array 2 = [" + array2 + "] <br>";
+    texto = texto + "Soma = [" + soma + "] <br>";
+    document.getElementById("resultado").innerHTML = texto;
+}

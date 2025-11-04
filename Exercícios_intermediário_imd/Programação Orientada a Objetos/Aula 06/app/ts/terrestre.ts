5d113dacf153c8340ffeff5bba35db570a2d9bd5
Jose <henrique.cg@outlook.com>
2025-11-04 16:19:57 -0300

    Aula 6 terminada

---
+++ Exercícios_intermediário_imd/Programação Orientada a Objetos/Aula 06/app/ts/terrestre.ts
@@ -0,0 +1,18 @@
+import { Transporte } from "./transporte";
+
+export class Terrestre extends Transporte {
+    private _numRodas : number;
+    
+    public get numRodas() : number {
+        return this._numRodas;
+    }
+    
+    
+    public set numRodas(numRodas : number) {
+        this._numRodas = numRodas;
+    }
+    
+    public exibirInformacoes(){
+            console.log(`Capacidade: ${this.capacidade}.\nRodas ${this._numRodas}`);
+    }
+}

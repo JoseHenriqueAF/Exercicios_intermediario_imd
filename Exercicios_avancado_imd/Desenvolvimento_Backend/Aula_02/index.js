a4a7ba5d2f67438292f0cce42d22e51c192ea1b6
Jose <henrique.cg@outlook.com>
2026-03-11 14:39:09 -0300

    Aula 02 backend terminada

---
+++ Exercicios_avancado_imd/Desenvolvimento_Backend/Aula_02/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+// const data = fs.readFileSync('./tmp.txt', {encoding:'utf-8', flag:'r'});
+// console.log(data);
+
+fs.readFile("/.tmp.txt",{encoding: 'utf-8', flag:'r'}, function(err, data){
+    if(!err){
+        console.log(data);
+    }
+});
+console.log("Executou aqui!");

eb067d8950ec9146dbbc463134beb57973becad3
Jose <henrique.cg@outlook.com>
2026-04-02 15:59:12 -0300

    Backend aula 09 terminada

---
+++ Exercicios_avancado_imd/Desenvolvimento_Backend/Aula_09/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const rotaUsuario = require('./rotas/usuario.rota');
+const rotaPost = require('./rotas/posts.rota');
+
+const app =  express();
+app.use(express.json());
+
+app.use('/static', express.static('public'))
+
+app.use('/usuarios', rotaUsuario);
+app.use('/posts', rotaPost);
+
+app.get('/', (req, res)=> {
+    res.json({msg: "Olá Mundo do Express!"})
+});
+
+
+
+app.listen(8080 , ()=>{
+    console.log(`Iniciando no ambiente ${process.env.NODE_ENV}`)
+    console.log("Servidor pronto na porta 8080")
+});

f60b25d2f8b54ba442bd82a25e2a8d2754de9c3a
Jose <henrique.cg@outlook.com>
2026-04-08 17:47:07 -0300

    Aula 11 terminada backend

---
+++ Exercicios_avancado_imd/Desenvolvimento_Backend/Aula_11/src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const rotaUsuario = require('./rotas/usuario.rota');
+const rotaPost = require('./rotas/posts.rota');
+var expressLayouts = require('express-ejs-layouts')
+
+const indexRoute = require('./rotas/index.rota')
+
+
+const app =  express();
+app.use(express.json());
+app.set('view engine', 'ejs')
+
+app.set('layout','layouts/layout')
+                  
+
+app.use(expressLayouts)
+
+app.use('/static', express.static('public'))
+
+app.use('/api/usuarios', rotaUsuario);
+app.use('/api/posts', rotaPost);
+app.use('/', indexRoute)
+
+
+app.get('/api', (req, res)=> {
+    res.json({msg: "Olá Mundo do Express!"})
+});
+
+
+app.listen(8080 , ()=>{
+    console.log(`Iniciando no ambiente ${process.env.NODE_ENV}`)
+    console.log("Servidor pronto na porta 8080")
+});

df24b5f02b9695d2efd9d038c43b8e55663e9391
Jose <henrique.cg@outlook.com>
2026-04-23 14:01:03 -0300

    Aula 14 backend terminada

---
+++ Exercicios_avancado_imd/Desenvolvimento_Backend/Aula_14/src/middleware/autenticacao.mid.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+function autenticar(req, res, next){
+    const auth = req.headers["authorization"]
+    const token = auth && auth.split(' ')[1]
+    console.log(token)
+
+    if (!token){
+        return res.sendStatus(401)
+    } else{
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err, payload) =>{
+            if(err) return res.sendStatus(403)
+            req.user = payload
+            next()
+        })
+    }
+}
+
+module.exports = autenticar
---
+++ Exercicios_avancado_imd/Desenvolvimento_Backend/Aula_14/src/middleware/validar.Post.middleware.js
@@ -0,0 +1,20 @@
+const Ajv = require('ajv');
+const ajv = new Ajv();
+const postSchema = require('../schema/post.schema');
+
+function validarPost(req, res, next) {
+    const post = req.body;
+    if (post.userId){
+        post.userId = Number(post.userId)
+    }
+    const validate = ajv.compile(postSchema);
+    const valid = validate(post);
+
+    if (valid){
+            next();
+    } else {
+        res.status(400).json({msg: "Dádos inválidos", errors: validate.errors});
+    }
+}
+
+module.exports = validarPost
